958f70e53d0aa5addcfdf7c391bfd3081119c4af
Niel Swusten <r0938850@student.thomasmore.be>
2023-12-17 18:08:58 +0100

    finetuning

---
+++ project/Migrations/20231217170844_UpdateUserWithFastestTime2.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace project.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserWithFastestTime2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_FastestTimes",
+                table: "FastestTimes");
+
+            migrationBuilder.RenameTable(
+                name: "FastestTimes",
+                newName: "FastestTime");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_FastestTime",
+                table: "FastestTime",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_FastestTime",
+                table: "FastestTime");
+
+            migrationBuilder.RenameTable(
+                name: "FastestTime",
+                newName: "FastestTimes");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_FastestTimes",
+                table: "FastestTimes",
+                column: "Id");
+        }
+    }
+}
